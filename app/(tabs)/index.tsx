f7410c384a3bd760a17d664dfbcc23d78391447e
ravmax-rdl <ravindudl.rc@gmail.com>
2025-04-12 21:56:11 +0530

    refactor: Update styles for feed screen and add sign out functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,18 @@
 import { Text, TouchableOpacity, View } from 'react-native';
-import { styles } from '../../Styles/auth.styles';
+import { styles } from '../../Styles/feed.styles';
 import { Link } from 'expo-router';
+import { useAuth } from '@clerk/clerk-expo';
+import { Ionicons } from '@expo/vector-icons';
+import { COLORS } from '@/constants/theme';
 
 export default function Index() {
+  const { signOut } = useAuth();
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello</Text>
-      <Link href="/notifications">Feed</Link>
+      <Text style={styles.header}>spotlight</Text>
+      <TouchableOpacity onPress={() => signOut()}>
+        <Ionicons name='log-out-outline' size={24} color={COLORS.white} />
+      </TouchableOpacity>
     </View>
   );
 }
