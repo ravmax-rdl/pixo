6d00d41f9cc21967f85994b8897e83caf4e00ed2
ravmax-rdl <ravindudl.rc@gmail.com>
2025-04-13 09:29:23 +0530

    refactor: Update feed screen styles and add sign out functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,63 @@
-import { Text, TouchableOpacity, View } from 'react-native';
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { styles } from '../../Styles/feed.styles';
 import { Link } from 'expo-router';
 import { useAuth } from '@clerk/clerk-expo';
 import { Ionicons } from '@expo/vector-icons';
 import { COLORS } from '@/constants/theme';
+import { STORIES } from '@/constants/mock-data';
+import Story from '@/components/Story';
+import { useQuery } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+import { Loader } from '@/components/Loader';
 
 export default function Index() {
   const { signOut } = useAuth();
+
+  const posts = useQuery(api.posts.getFeedPosts);
+
+  if (posts === undefined) return <Loader />;
+
+  if (posts.length === 0) return <NoPostsFound />;
+
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>spotlight</Text>
-      <TouchableOpacity onPress={() => signOut()}>
-        <Ionicons name='log-out-outline' size={24} color={COLORS.white} />
-      </TouchableOpacity>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>spotlight</Text>
+        <TouchableOpacity onPress={() => signOut()}>
+          <Ionicons name="log-out-outline" size={24} color={COLORS.white} />
+        </TouchableOpacity>
+      </View>
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={styles.storiesContainer}
+        >
+          {STORIES.map((story) => (
+            <Story key={story.id} story={story} />
+          ))}
+        </ScrollView>
+
+        {posts.map((post) => (
+          <Post key={post._id} post={post} />
+        ))}
+      </ScrollView>
     </View>
   );
 }
+
+const NoPostsFound = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: COLORS.background,
+      }}
+    >
+      <Text style={{ fontSize: 20, color: COLORS.primary }}>No posts yet</Text>
+    </View>
+  );
+};
---
+++ components/Story.tsx
@@ -0,0 +1,19 @@
+import { styles } from '@/Styles/feed.styles';
+import { TouchableOpacity, View, Image, Text } from 'react-native';
+
+type Story = {
+  id: string;
+  username: string;
+  avatar: string;
+  hasStory: boolean;
+};
+
+export default function Story({ story }: { story: Story }) {
+  return (
+    <TouchableOpacity style={styles.storyWrapper}>
+      <View style={[styles.storyRing, !story.hasStory && styles.noStory]}>
+        <Image source={{ uri: story.avatar }} style={styles.storyAvatar} />
+      </View>
+    </TouchableOpacity>
+  );
+}
